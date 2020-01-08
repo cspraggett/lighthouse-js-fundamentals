e38b8b9a5b3cdd983ce6f92a02a550122cf4815a
cspraggett <cspraggett@gmail.com>
2020-01-08 00:03:07 +0000

    some changes

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -1,15 +1,34 @@
-function loopyLighthouse(range, multiples, words) {
-  for (let i = range[0]; i <= range[1]; i++) {
-    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
-      console.log(words[0] + words[1]);
-    } else if (i % multiples[0] === 0) {
-      console.log(words[0]);
-    } else if (i % multiples[1] === 0) {
-      console.log(words[1]);
-    } else {
-      console.log(i);
-    }
+// function loopyLighthouse(range, multiples, words) {
+//   for (let i = range[0]; i <= range[1]; i++) {
+//     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+//       console.log(words[0] + words[1]);
+//     } else if (i % multiples[0] === 0) {
+//       console.log(words[0]);
+//     } else if (i % multiples[1] === 0) {
+//       console.log(words[1]);
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+let getChar = (num, multiples, words) => {
+  if (num % multiples[0] === 0 && num % multiples[1] === 0) {
+    return words[0] + words[1];
   }
-}
+  if (num % multiples[0] === 0) {
+    return words[0];
+  }
+  if (num % multiples[1] === 0) {
+    return words[1];
+  }
+  return num;
+};
+
+let loopyLighthouse = (range, multiples, words) => {
+  for (let i = range[0]; i <= range[1]; i++) {
+    console.log(getChar(i, multiples, words));
+  }
+};
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
